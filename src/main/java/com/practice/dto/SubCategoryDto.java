7237bf06397798546e4735d11c54acfc055583b1
pp_ patil7 <pundalikpatil7218@gmail.com>
2025-08-14 15:15:07 +0530

    boilerplate

--- src/main/java/com/practice/dto/SubCategoryDto.java
+++ src/main/java/com/practice/dto/SubCategoryDto.java
@@ -1,5 +1,14 @@
 package com.practice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class SubCategoryDto {
 
     private Long subCategoryId;
